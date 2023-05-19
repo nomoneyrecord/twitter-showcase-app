1af510ef923e631af5a8b526d3f8ab355f206c77
Gary H <3rdeye.ghj@gmail.com>
2023-05-19 15:35:10 -0700

    Developing navbar and basic wireframe

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>Random Tweet</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
