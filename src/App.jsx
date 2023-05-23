04973f76d38f10bfeed7935203a39eb2d61b6799
Gary H <3rdeye.ghj@gmail.com>
2023-05-23 14:26:49 -0700

    Changed icon size and link spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import NavbarOne from './components/Navbar'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Home from "./Pages/Home"
+import Search from "./Pages/Search"
+import Random from "./Pages/Random"
 
 
 
@@ -10,12 +13,12 @@ function App() {
   let component
   switch (window.location.pathname) {
     case '/':
-      component = <App />
+      component = <Home />
       break;
     case '/Search':
       component = <Search />
       break;
-    case 'Random':
+    case '/Random':
       component = <Random /> 
       break;
     }
