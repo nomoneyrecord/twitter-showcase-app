2128e2f9daba3f8918229cef92c2bd7f08032752
Gary H <3rdeye.ghj@gmail.com>
2023-05-24 13:54:18 -0700

    Working on flip cards and active links to pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,28 @@ export default function Home() {
         <h1 className="main-head">TWITTER SHOWCASE</h1>
         <h4 className="sub-head">A simple twitter app to filter out the noise</h4>
       </div>
+      <div className="flip-container">
+        <div className="flip-box">
+          <div className="flip-box-inner">
+            <div className="flip-box-front">
+              <img className="magnifier" src="./images/magnifying-glass.png" alt="magnifying glass with search word" />
+            </div>
+            <div className="flip-box-back">
+              <h3>Click the search button to explore recents tweets</h3>
+            </div>
+          </div>
+        </div>
+        <div className="flip-box">
+          <div className="flip-box-inner">
+            <div className="flip-box-front">
+              <img className="question-mark" src="./images/blue-question-mark.png" alt="" />
+            </div>
+            <div className="flip-box-back">
+              <h3>Click the button below to search tweets through my top 5 tweeters</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Search() {
-
+  <>
+  <div>
+    <h3>Search the most recent tweets</h3>
+  </div>
+  </>
 }
