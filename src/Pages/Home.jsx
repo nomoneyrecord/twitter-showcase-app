d4c73f997731f2fdf5795a06dca9557777d66955
Gary H <3rdeye.ghj@gmail.com>
2023-05-24 09:53:03 -0700

    Homepage header added and working on gradient

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="header">
+      <h1>TWITTER SHOWCASE</h1>
+      <h4>A simple twitter app to filter out the noise</h4>
+    </div>
   )
 }
--- src/Pages/Random.jsx
+++ src/Pages/Random.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 
 export default function Random() {
-  return (
-  <h1>Random</h1>
-  )
+
 }
