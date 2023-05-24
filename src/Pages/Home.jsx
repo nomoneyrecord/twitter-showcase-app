365a8408c79975a534b8e7d6b00b4b60b9bb6fe1
Gary H <3rdeye.ghj@gmail.com>
2023-05-24 13:12:11 -0700

    Scratched card idea and simplifying with linked images

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,28 +7,6 @@ export default function Home() {
         <h1 className="main-head">TWITTER SHOWCASE</h1>
         <h4 className="sub-head">A simple twitter app to filter out the noise</h4>
       </div>
-      <div className="flip-container">
-        <div class="flip-box">
-          <div class="flip-box-inner">
-            <div class="flip-box-front">
-              <h2>Front Side</h2>
-            </div>
-            <div class="flip-box-back">
-              <h2>Back Side</h2>
-            </div>
-          </div>
-        </div>
-        <div class="flip-box">
-          <div class="flip-box-inner">
-            <div class="flip-box-front">
-              <h2>Front Side</h2>
-            </div>
-            <div class="flip-box-back">
-              <h2>Back Side</h2>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
