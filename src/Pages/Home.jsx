0a5ec5b323bf6b48add774ebbe63ee779c96384c
Gary H <3rdeye.ghj@gmail.com>
2023-05-24 16:51:01 -0700

    Buttons with animations added and flip cards almost completed on backside

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,28 +5,52 @@ export default function Home() {
     <>
       <div className="header">
         <h1 className="main-head">TWITTER SHOWCASE</h1>
-        <h4 className="sub-head">A simple twitter app to filter out the noise</h4>
+        <h4 className="sub-head">
+          A simple twitter app to filter out the noise
+        </h4>
       </div>
       <div className="flip-container">
         <div className="flip-box">
           <div className="flip-box-inner">
             <div className="flip-box-front">
-              <img className="magnifier" src="./images/magnifying-glass.png" alt="magnifying glass with search word" />
+              <img
+                className="magnifier"
+                src="./images/magnifying-glass.png"
+                alt="magnifying glass with search word"
+              />
             </div>
             <div className="flip-box-back">
-              <h3>Click the search button to explore recents tweets</h3>
+              <h3 >Search recent tweets</h3>
             </div>
           </div>
         </div>
         <div className="flip-box">
           <div className="flip-box-inner">
             <div className="flip-box-front">
-              <img className="question-mark" src="./images/blue-question-mark.png" alt="" />
+              <img
+                className="question-mark"
+                src="./images/speech-bubble.png"
+                alt=""
+              />
             </div>
             <div className="flip-box-back">
-              <h3>Click the button below to search tweets through my top 5 tweeters</h3>
+              <h3>
+                Get a random tweet from 5 of my favorite minds
+              </h3>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="glow-btns">
+        <div className="search-btn">
+          <button class="glow-on-hover" type="button">
+            Search
+          </button>
+        </div>
+        <div className="random-btn">
+          <button class="glow-on-hover" type="button">
+            Random
+          </button>
         </div>
       </div>
     </>
