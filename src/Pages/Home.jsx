d0a762968565ca5e3a6a864102c7ca8c7e5fce5a
Gary H <3rdeye.ghj@gmail.com>
2023-05-24 10:36:10 -0700

    Glowing header completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Home() {
   return (
     <div className="header">
-      <h1>TWITTER SHOWCASE</h1>
+      <h1 className="main-head">TWITTER SHOWCASE</h1>
       <h4>A simple twitter app to filter out the noise</h4>
     </div>
   )
