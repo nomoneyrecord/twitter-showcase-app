8f059539dfb9c1bda2e897f36f215b2d4726ec29
Gary H <3rdeye.ghj@gmail.com>
2023-06-07 14:04:51 -0700

    Building search functionality prior to connect to API

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useRef } from "react";
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const inputRef = useRef();
+
+  const handleInputChange = () => {
+    setSearchTerm(inputRef.current.value);
+  };
+
+  const handleSearch = () => {
+    console.log("Search term is : ", searchTerm);
+    //API request functon should be called here, apiRequest(searchTerm);
+  };
+
   return (
     <>
       <div className="search-header">
         <h3>Search the most recent tweets</h3>
       </div>
       <div className="input-container">
-      <input className="input" type="text" placeholder="enter keyword"/>
-      <button className="search-btn">Search</button>
+        <input
+          ref={inputRef}
+          id="search-input"
+          className="input"
+          type="text"
+          placeholder="enter keyword"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button className="search-btn" onClick={handleSearch}>Search</button>
       </div>
     </>
   );
