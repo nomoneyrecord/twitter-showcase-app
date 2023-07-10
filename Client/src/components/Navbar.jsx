481d0c047fde7916319a824f2a9772a99f354385
Gary H <3rdeye.ghj@gmail.com>
2023-07-10 11:02:01 -0700

    Drop down meny not working as intended and will style after functionality is completed

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,48 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
   return (
     <div>
-      <nav id="navbar" className="navbar navbar-dark navbar-expand-lg">
-        <a href="/" className="navbar-brand">
-          <img src="/images/twitter-icon.jpeg" />
+      <nav id="navbar" className="navbar navbar-dark navbar-expand-lg ">
+        <a href="/" className="navbar-brand"><img src="/images/twitter-icon.jpeg" />
         </a>
-        <div className="ml-auto collapse navbar-collapse" id="navbarCollapse">
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={toggle}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className={`navbar-nav ${isOpen ? "show" : ""}`}>
-            <ul className="navbar-nav">
-              <li className="navbar-item d-none d-lg-block">
-                <a href="/Search" className="nav-link">
-                  Search
-                </a>
-              </li>
-              <li className="navbar-item d-none d-lg-block">
-                <a href="/Random" className="nav-link">
-                  Random
-                </a>
-              </li>
-              <li className="navbar-item d-lg-none">
-                <a href="/Search" className="nav-link dropdown-link">
-                  Search
-                </a>
-              </li>
-              <li className="navbar-item d-lg-none">
-                <a href="/Random" className="nav-link dropdown-link">
-                  Random
-                </a>
-              </li>
-            </ul>
-          </div>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav ml-auto navbar-white">
+            <li className="navbar-item">
+              <a href="/Search" className="nav-link">
+                Search 
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="/Random" className="nav-link">
+                Random
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
