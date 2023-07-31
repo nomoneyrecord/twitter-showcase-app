8caeb558d4b16cf6f59e87257f3e9f704bfee1b5
Gary H <3rdeye.ghj@gmail.com>
2023-07-31 13:28:24 -0700

    Images rendering and now working on styling

--- Client/src/Pages/Search.jsx
+++ Client/src/Pages/Search.jsx
@@ -63,22 +63,31 @@ export default function Search() {
           </button>
         </form>
       </div>
-      {searched === true && tweets.length === 0 && (
-        <div className="no-tweets-message">
-          No tweets found for the given username.
-        </div>
-      )}
       {searched === true && tweets.length > 0 && (
         <div className="tweet-cards">
           {tweets.map((tweet) => (
-            <div key={tweet.id} className="card">
-              {tweet.profile_image_url_https && (
-                <img src={tweet.profile_image_url_https} alt="Profile" />
-              )}
-              <h4>{tweet.username}</h4>
-              <p>{tweet.text}</p>
-              <p>Likes: {tweet.like_count}</p>
-              <p>Retweets: {tweet.retweet_count}</p>
+            <div key={tweet.id} className="card mb-3">
+              <div className="card-header d-flex align-items-center">
+                {tweet.profile_image_url_https && (
+                  <img
+                    src={tweet.profile_image_url_https}
+                    alt="Profile"
+                    className="profile-image img-fluid rounded-circle me-2"
+                    style={{ maxWidth: "40px" }}
+                  />
+                )}
+                <h4 className="mb-0">{tweet.username}</h4>
+              </div>
+              <div className="card-body">
+                <p>{tweet.text}</p>
+              </div>
+              <div className="card-footer d-flex justify-content-between">
+                <div>
+                  <span className="me-2">Likes: {tweet.like_count}</span>
+                  <span>Retweets: {tweet.retweet_count}</span>
+                </div>
+                {/* Add additional elements as needed for the footer */}
+              </div>
             </div>
           ))}
         </div>
