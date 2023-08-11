873400419572d8df6f67a3e4006a4c45fdc20a99
Gary H <3rdeye.ghj@gmail.com>
2023-08-11 13:54:12 -0700

    Fixed bug with valid username prompt

--- Client/src/Pages/Search.jsx
+++ Client/src/Pages/Search.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./Search.css";
 
@@ -25,6 +25,7 @@ export default function Search() {
       });
 
       if (response.data.length === 0) {
+        // If the response data is an empty array, it means no tweets were found for the given username
         setTweets([]);
         setSearched(true);
       } else {
@@ -69,49 +70,55 @@ export default function Search() {
           </button>
         </form>
       </div>
-      {searched === true && tweets.length > 0 && (
-        <div className="tweet-cards">
-          {tweets.map((tweet) => (
-            <div key={tweet.id} className="card mb-3">
-              <div className="card-header d-flex align-items-center">
-                {tweet.profile_image_url && (
-                  <img
-                    src={tweet.profile_image_url}
-                    alt="Profile"
-                    className="profile-image img-fluid rounded-circle me-2"
-                    style={{ maxWidth: "40px" }}
-                  />
-                )}
-                <h4 className="mb-0">{tweet.username}</h4>
-              </div>
-              <div className="card-body">
-                <p>{tweet.text}</p>
-                
-                {/* Render attached media */}
-                {tweet.media_urls && tweet.media_urls.map((url, index) => (
-                  <img key={index} src={url} alt="Tweet Media" className="tweet-media img-fluid rounded mt-2" />
-                ))}
-              </div>
-              <div className="card-footer d-flex justify-content-between">
-                <div>
-                  <img
-                    src="/images/like_icon.png"
-                    alt="Like Icon"
-                    className="icon-img"
-                  />
-                  <span className="me-2">{tweet.like_count}</span>
-                  <img
-                    src="/images/retweet_icon.png"
-                    alt="Retweet Icon"
-                    className="icon-img"
-                  />
-                  <span>{tweet.retweet_count}</span>
-                </div>
-              </div>
-            </div>
+      {searched === true && tweets.length > 0 ? (
+  <div className="tweet-cards">
+    {tweets.map((tweet) => (
+      <div key={tweet.id} className="card mb-3">
+        <div className="card-header d-flex align-items-center">
+          {tweet.profile_image_url && (
+            <img
+              src={tweet.profile_image_url}
+              alt="Profile"
+              className="profile-image img-fluid rounded-circle me-2"
+              style={{ maxWidth: "40px" }}
+            />
+          )}
+          <h4 className="mb-0">{tweet.username}</h4>
+        </div>
+        <div className="card-body">
+          <p>{tweet.text}</p>
+
+          {/* Render attached media */}
+          {tweet.media_urls && tweet.media_urls.map((url, index) => (
+            <img key={index} src={url} alt="Tweet Media" className="tweet-media img-fluid rounded mt-2" />
           ))}
         </div>
-      )}
+        <div className="card-footer d-flex justify-content-between">
+          <div>
+            <img
+              src="/images/like_icon.png"
+              alt="Like Icon"
+              className="icon-img"
+            />
+            <span className="me-2">{tweet.like_count}</span>
+            <img
+              src="/images/retweet_icon.png"
+              alt="Retweet Icon"
+              className="icon-img"
+            />
+            <span>{tweet.retweet_count}</span>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+) : searched === true ? (
+  <div className="no-tweets-container">
+    <div className="no-tweets-message">
+      Please use a valid username
+    </div>
+  </div>
+) : null}
     </>
   );
 }
